8de97fc737b57e4ac65c84331d098e3c2b577aa3
hulklee <hulklee@gmail.com>
2023-07-13 16:51:46 +0900

    0713-2

--- poinerSample/poinerSample/poinerSample.cpp
+++ poinerSample/poinerSample/poinerSample.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <malloc.h>
 
-char buf[200];
+char buf[2000];
 void MemoryDump(int start, int length); // 함수의 원형 Prototype
 
 int main(int argc, char *argv[])	// >PointerSample 10000 500 : //Command Line 명령어
